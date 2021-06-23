040ffc6f4e6798fb58ffe834566ee190b0d9c11a
aaronlyy <aaronlevican@gmail.com>
2021-06-23 19:42:00 +0200

    added first methods

--- memoria/memoria.cpp
+++ memoria/memoria.cpp
@@ -1,1 +1,33 @@
 #include "memoria.hpp"
+
+#include <string>
+#include <Windows.h>
+
+// --- Handle Methods + PID ---
+HWND Memoria::GetWindowHandle(LPCSTR wName)
+{
+	HWND wHandle = FindWindowA(0, wName); // get handle to top level window
+	return wHandle;
+}
+
+DWORD Memoria::GetProcessId(HWND wHandle)
+{
+	DWORD pid;
+	GetWindowThreadProcessId(wHandle, &pid); // get the process id using window handle
+	return pid;
+}
+
+HANDLE Memoria::GetProcessHandle(DWORD pid, DWORD access)
+{
+	// access: (https://docs.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights)
+	HANDLE pHandle = OpenProcess(access, FALSE, pid); // get process handle
+	return pHandle;
+}
+
+// --- Reading Memory ---
+int Memoria::ReadInt(HANDLE pHandle, DWORD address)
+{
+	int val;
+	ReadProcessMemory(pHandle, (void*)address, &val, sizeof(val), 0);
+	return val;
+}
--- memoria/memoria.hpp
+++ memoria/memoria.hpp
@@ -1,5 +1,21 @@
 #pragma once
+
+#include <string>
+
+#include <Windows.h>
+
+
 class Memoria
 {
+public:
+	// handle methods + pid
+	HWND GetWindowHandle(LPCSTR wName);
+	DWORD GetProcessId(HWND wHandle);
+	HANDLE GetProcessHandle(DWORD pid, DWORD access = PROCESS_ALL_ACCESS);
+
+	// reading methods
+	int ReadInt(HANDLE pHandle, DWORD address);
+
+	// writing methods
 };
 
